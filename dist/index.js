8967a8e80df0a65cf07a5c0a31feb2d3ad11165a
Lsunshineandrainbow <3480782290@qq.com>
2018-08-06 13:06:11 +0800

    ES6

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var a = 2;
+var b = 6;
+var fn = function fn(a, b) {
+    return a * b;
+};
+var c = 7;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+let a = 2;
+const b =6;
+let fn=(a,b)=>{
+    return a*b;
+}
+let c = 7;
