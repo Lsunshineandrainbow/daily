40d95c39c3f066b85a547a8dd907e2d645261f0c
Lsunshineandrainbow <3480782290@qq.com>
2018-09-13 20:36:35 +0800

    React

---
+++ wangyiyun/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <ul class="header">
+
+    </ul>
+</div>
+</template>
+<script>
+
+</script>
+<style  lang="scss">
+div{
+
+}
+</style>
---
+++ wangyiyun/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul class="header">
+            <li v-for="val in musiclist">111
+                <svg class="icon" aria-hidden="true">
+                    <use :xlink:href='val.font'>{{val.font}}</use>
+                </svg>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export  default {
+        props:['musiclist']
+    }
+
+</script>
+<style  lang="scss">
+    .icon {
+        width: 1em; height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
