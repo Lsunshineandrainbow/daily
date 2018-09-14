9796928d7efab34a9337584dd72b3e73f154a2b9
Lsunshineandrainbow <3480782290@qq.com>
2018-09-14 19:51:35 +0800

    React

--- wangyiyun/src/components/Footer.vue
+++ wangyiyun/src/components/Footer.vue
@@ -1,11 +1,25 @@
 <template>
 <div>
-    <ul class="header">
-
-    </ul>
+    <div id="aplayer"></div>
 </div>
 </template>
 <script>
+    import 'APlayer/dist/APlayer.min.css';
+    import APlayer from 'APlayer';
+    var ap = new APlayer({
+
+    });
+export default {
+    data(){},
+    created(){
+        Axios.get('./data/musicdata.json').then((res)=>{
+            var data = res.data;
+            console.log(data);
+            console.log(111)
+        })
+    }
+
+}
 
 </script>
 <style  lang="scss">
