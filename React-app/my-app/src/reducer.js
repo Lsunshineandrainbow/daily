3d77159cb88a2e3cd49405dc2580faca5c2eb662
Lsunshineandrainbow <3480782290@qq.com>
2018-09-16 12:19:25 +0800

    reactx

--- React-app/my-app/src/reducer.js
+++ React-app/my-app/src/reducer.js
@@ -1,1 +1,13 @@
 import {combineReducers} from 'redux';
+const count =(state=0,action)=>{
+    switch (action.type){
+        case 'ADD ':
+            return state+1;
+        default:
+            return  state;
+    }
+};
+const reducer = combineReducers({
+    count
+});
+export default reducer;
