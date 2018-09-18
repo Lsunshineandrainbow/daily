ae3763cac55d733bd88dae7c15f1ebdaa793d0dc
Lsunshineandrainbow <3480782290@qq.com>
2018-09-18 21:02:33 +0800

    React

---
+++ wangyiyun/src/views/Home/Homepage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Header :musiclist="musiclist"></Header>
+        <router-view></router-view>
+    </div>
+
+</template>
+<script>
+    import Header from './Header'
+    export default {
+        props:['musiclist'],
+        components:{
+            Header
+        }
+    }
+
+</script>
+<style>
+
+</style>
