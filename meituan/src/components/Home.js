232282085d106675edb4961f098620bae1ad3647
Lsunshineandrainbow <3480782290@qq.com>
2018-09-22 22:45:56 +0800

    ReactApp

---
+++ meituan/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { NavBar, Icon } from 'antd-mobile';
+
+export default  class Home extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                111
+            </div>
+        )
+    }
+}
