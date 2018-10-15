131bbe28cee69dadcfa1dfc5d93162b6945a1444
Lsunshineandrainbow <3480782290@qq.com>
2018-10-15 23:54:38 +0800

    美团

---
+++ meituan/src/components/Detail/DImg.js
@@ -0,0 +1,17 @@
+import  React,{Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import 'whatwg-fetch';
+import Detail from "../Detail";
+export default withRouter(
+    class DImg extends React.Component{
+        constructor(props){
+            super(props)
+        }
+
+        render(){
+            return(
+                <div>111</div>
+            )
+        }
+    }
+)
